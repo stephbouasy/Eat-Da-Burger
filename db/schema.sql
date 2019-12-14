ebb5425df9f81e9064b9cb42cad12fc4e0f77c15
sbouasy <stephbouasy@gmail.com>
2019-12-14 15:59:44 -0600

    updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
-CREATE TABLE burgers_db
+CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN default FALSE,
+    name VARCHAR(255) NOT NULL,
+    bite BOOLEAN default FALSE,
     PRIMARY KEY (id)
 );
